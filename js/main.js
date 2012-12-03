e1f06a1e0fa43574490bbabda29870b21d4928ed
benvds <ben@pomodoro.nl>
2012-12-03 23:04:39 +0100

    use transitions instead of animations, fix flickering

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
 (function ($) {
 
-    // function setBackground () {
-    //     // $.backstretch(
-    //     //               "img/backgrounds/coding.jpg",
-    //     //               "img/backgrounds/realcaptain.jpg",
-    //     //               "img/backgrounds/golden.jpg"
-    //     // ], {duration: 2 * 1000});
-    //     $.backstretch('img/backgrounds/coding.jpg');
-    // }
-
     // window load
     $(window).on('load', function () {
-        // setBackground();
-        // window.setTimeout(function() { $('.backstretch').addClass('animation-blur'); }, 3 * 1000);
+            $('body').addClass('window-loaded');
     });
 
 })(jQuery);

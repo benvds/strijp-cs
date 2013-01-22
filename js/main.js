14388494794ddec4c5ac8af9f15570626967c08e
benvds <ben@pomodoro.nl>
2013-01-22 14:13:49 +0100

    added arrows, updated css

--- js/main.js
+++ js/main.js
@@ -5,4 +5,18 @@
             $('body').addClass('window-loaded');
     });
 
+    $('.asset').each(function() {
+		var $this = $(this),
+			assetName = $this.data('asset'),
+			$original = $('#assets').find('.' + assetName + ':first');
+
+		if ($original) {
+			$copy = $original.clone();
+			$copy.width($this.width());
+			$copy.height($this.height());
+			$this.append($copy).addClass('asset-loaded');
+		}
+    });
+
+
 })(jQuery);

3d63f66a9186f1b4b55f12a7f086fa17c49e77c6
benvds <ben@pomodoro.nl>
2013-01-28 20:18:22 +0100

    removed grunt

--- src/documents/js/main.js
+++ src/documents/js/main.js
@@ -1,42 +1,42 @@
 (function ($) {
 
-	function setGalleryWidths() {
-		$('ul.gallery').each(function() {
-			var $this = $(this),
-				$lis = $this.children('li'),
-				paddingLeft = parseInt($this.css('paddingLeft'), 10),
-				paddingRight = parseInt($this.css('paddingRight'), 10),
-				totalWidth = 0;
+    function setGalleryWidths() {
+        $('.gallery-overview').each(function() {
+            var $this = $(this),
+            $lis = $this.children('li'),
+            paddingLeft = parseInt($this.css('paddingLeft'), 10),
+            paddingRight = parseInt($this.css('paddingRight'), 10),
+            totalWidth = 0;
 
-			console.log('li', $this);
+            console.log('li', $this);
 
-			$lis.each(function() {
-				totalWidth += $(this).outerWidth();
-			});
+            $lis.each(function() {
+                totalWidth += $(this).outerWidth();
+            });
 
-			$this.width(totalWidth + paddingLeft + paddingRight);
-		});
-	}
+            $this.width(totalWidth + paddingLeft + paddingRight);
+        });
+    }
 
-	function loadAssets() {
-		$('.asset').each(function() {
-			var $this = $(this),
-				assetName = $this.data('asset'),
-				$original = $('#assets').find('.' + assetName + ':first');
+    function loadAssets() {
+        $('.asset').each(function() {
+            var $this = $(this),
+            assetName = $this.data('asset'),
+            $original = $('#assets').find('.' + assetName + ':first');
 
-			if ($original) {
-				$copy = $original.clone();
-				$copy.width($this.width());
-				$copy.height($this.height());
-				$this.append($copy).addClass('asset-loaded');
-			}
-	    });
-	}
+            if ($original) {
+                $copy = $original.clone();
+                $copy.width($this.width());
+                $copy.height($this.height());
+                $this.append($copy).addClass('asset-loaded');
+            }
+        });
+    }
 
-	// document load
-	$(function () {
-    	setGalleryWidths();
-    	loadAssets();
+    // document load
+    $(function () {
+        setGalleryWidths();
+        loadAssets();
     });
 
     // window load

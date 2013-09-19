1cf4b58bbb7fa6a0af323bf370832055fb7b6e3e
benvds <ben@pomodoro.nl>
2013-09-19 12:03:04 +0200

    contact & verhuur

--- src/documents/js/main.js
+++ src/documents/js/main.js
@@ -67,9 +67,65 @@
         }, slideshowInterval);
     }
 
+    function loadMap() {
+        var latlng    = new google.maps.LatLng(51.44571601895113, 5.458906888961792);
+
+        var mapStyles = [
+            {
+                "featureType": "landscape",
+                "stylers": [
+                    { "saturation": -100 }
+                ]
+            },{
+                "featureType": "water",
+                "stylers": [
+                    { "gamma": 9.99 },
+                    { "lightness": 100 }
+                ]
+            },{
+                "featureType": "poi",
+                "stylers": [
+                    { "hue": "#ffee00" },
+                    { "saturation": 50 }
+                ]
+            }
+        ];
+
+        var styledMap = new google.maps.StyledMapType(mapStyles,
+                                                      {name: "styled_map"});
+
+        var myOptions = {
+            zoom: 15,
+            center: latlng
+        };
+
+        var map       = new google.maps.Map(document.getElementById("map"),
+                                            myOptions);
+
+        map.mapTypes.set('map_style', styledMap);
+        map.setMapTypeId('map_style');
+
+        var marker    = new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title: "Strijp-CS"
+        });
+
+        var infowindow = new google.maps.InfoWindow({
+            content: $('.map-info-window-content').html()
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+        infowindow.open(map, marker);
+    }
+
     // document load
     $(function () {
         loadAssets();
+        if($('#map').length) { loadMap(); }
     });
 
     // window load
